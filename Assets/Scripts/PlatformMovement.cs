c56b37816e12dc9683d8df0a68dbc4bc3fbd865b
JulianKirk <progamerguy005@gmail.com>
2024-07-17 21:37:56 +1000

    Made tilemap. Made simple platform movement script

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -1,23 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
 {
-    Rigidbody2D rigidbody;
+    public float Speed;
+
+    public float JumpSpeed;
+    public float JumpUpTime;
+    public float TypicalGravityScale;
+
+    float remainingJumpUpTime;
+    bool isJumping;
+
+    Rigidbody2D playerRigidbody;
+
+    public Transform GroundCheck;
+    public float GroundCheckRadius;
+    public LayerMask WhatIsGround;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody2D>();
+
+        remainingJumpUpTime = JumpUpTime;
+
+        isJumping = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var horizontalInput = Input.GetAxisRaw("horizontal");
-        var verticalInput = Input.GetAxisRaw("vertical");
+        remainingJumpUpTime -= Time.deltaTime;
 
-        rigidbody.velocity = new Vector3(horizontalInput, verticalInput, 0f);
+        var horizontalVelocity = Input.GetAxisRaw("Horizontal") * Speed;
+        var verticalVelocity = playerRigidbody.velocity.y;
+
+        if (IsGrounded())
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                verticalVelocity = JumpSpeed;
+                remainingJumpUpTime = JumpUpTime;
+                playerRigidbody.gravityScale = 0;
+
+                isJumping = true;
+            }
+        }
+        else
+        {
+            remainingJumpUpTime -= Time.deltaTime;
+
+            if (remainingJumpUpTime <= 0)
+            {
+                if (isJumping)
+                {
+                    isJumping = false;
+                }
+
+                playerRigidbody.gravityScale = TypicalGravityScale;
+            }
+        }
+
+        playerRigidbody.velocity = new Vector2(horizontalVelocity, verticalVelocity);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
     }
 }

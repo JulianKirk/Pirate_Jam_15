ef74bd40ad3987cffb548ec14392f923678ed254
JulianKirk <progamerguy005@gmail.com>
2024-07-22 23:32:00 +1000

    removed debug statements

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -30,25 +30,18 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         var camAngle = CamTarget.eulerAngles.y * Mathf.Deg2Rad;
-        Debug.Log($"Camera angle in radians: {camAngle}");
-        Debug.Log($"Original camera angle: {CamTarget.rotation.y}");
 
         var sinCam = Mathf.Sin(camAngle);
         var cosCam = Mathf.Cos(camAngle);
-        //Debug.Log(sinCam);
-        //Debug.Log(cosCam);
 
         Vector2 hReq = Speed * Vector2.ClampMagnitude(
             new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")), 1);
 
         var xVel = hReq.x * cosCam + hReq.y * sinCam;
         var yVel = -hReq.x * sinCam + hReq.y * cosCam;
-        //Debug.Log($"X Velocity: {xVel}");
-        //Debug.Log($"Y Velocity: {yVel}");
 
         Vector2 hVel = new Vector2(xVel, yVel);
         playerRigidbody.velocity = new Vector3(hVel.x, playerRigidbody.velocity.y, hVel.y);
-        //Debug.Log($"Velocity: {playerRigidbody.velocity}");
 
         if (Input.GetButton("Jump") && remainingJumpUpTime > 0)
         {

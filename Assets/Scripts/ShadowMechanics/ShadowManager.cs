0a7bd146eababc2bd0a5951ffd82798b7e40e696
JulianKirk <progamerguy005@gmail.com>
2024-07-24 00:09:41 +1000

    hooked up the manager and receiver

--- Assets/Scripts/ShadowMechanics/ShadowManager.cs
+++ Assets/Scripts/ShadowMechanics/ShadowManager.cs
@@ -11,24 +11,53 @@ public class LightManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        lightSources = new List<Light>();
+        lightSources = Object.FindObjectsOfType<Light>().ToList();
 
         var shadowReceivers = Object.FindObjectsOfType<ShadowReceiver>();
 
         foreach (var receiver in shadowReceivers)
         {
-            shadowReceiverStates[receiver] = true;
+            shadowReceiverStates[receiver] = CheckIfInShadow(receiver);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (var (receiver, wasInShadow) in shadowReceiverStates) 
+        {
+            var inShadow = CheckIfInShadow(receiver);
+
+            if (inShadow != wasInShadow)
+            {
+                if (inShadow == true)
+                {
+                    receiver.OnEnterShadow();
+                }
+                else
+                {
+                    receiver.OnExitShadow();
+                }
+            }
+        }
     }
 
-    void CheckIfInShadow(ShadowReceiver receiver)
+    bool CheckIfInShadow(ShadowReceiver receiver)
     {
+        foreach (var light in lightSources)
+        {
+            foreach (var target in receiver.ColliderCorners)
+            {
+                if (Physics.Raycast(light.transform.position, (target - light.transform.position).normalized, out RaycastHit hitInfo))
+                {
+                    if (hitInfo.transform.gameObject == receiver.gameObject) //If it hit a collider attached to the receiver game object
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
 
+        return false;
     }
 }
--- Assets/Scripts/ShadowMechanics/ShadowReceiver.cs
+++ Assets/Scripts/ShadowMechanics/ShadowReceiver.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class ShadowReceiver : MonoBehaviour
 {
-    public List<Vector3> ObjectCorners;
+    public List<Vector3> ColliderCorners;
 
     // Start is called before the first frame update
     void Awake()
     {
-        
+        var transform = gameObject.transform;
+
+        ColliderCorners.Add(transform.position);
+
+        var colliders = gameObject.GetComponents<Collider>();
+
+        foreach (var collider in colliders)
+        {
+            var center = collider.bounds.center;
+            var extents = collider.bounds.extents;
+
+            ColliderCorners.Add(center + new Vector3(extents.x, extents.y, extents.z)); //Equivalent to max
+            ColliderCorners.Add(center + new Vector3(extents.x, -extents.y, extents.z));
+            ColliderCorners.Add(center + new Vector3(extents.x, -extents.y, -extents.z));
+            ColliderCorners.Add(center + new Vector3(extents.x, extents.y, -extents.z));
+            ColliderCorners.Add(center + new Vector3(-extents.x, extents.y, extents.z));
+            ColliderCorners.Add(center + new Vector3(-extents.x, -extents.y, extents.z));
+            ColliderCorners.Add(center + new Vector3(-extents.x, extents.y, -extents.z));
+            ColliderCorners.Add(center + new Vector3(-extents.x, -extents.y, -extents.z)); //Equivalent to min
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public virtual void OnEnterShadow()
+    {
+    }
+    
+    public virtual void OnExitShadow()
+    {
     }
 }
